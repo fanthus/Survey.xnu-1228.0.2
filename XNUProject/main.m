82643cd4e8eee69a8992d60c364d9a70614de986
xiushan.fan <fanxiushan@smartisan.com>
2017-04-20 10:29:58 +0800

    rebuild xnu project.
    Signed-off-by: xiushan.fan <fanxiushan@smartisan.com>

---
+++ XNUProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XNUProject
+//
+//  Created by xiushan.fan on 2017/4/20.
+//  Copyright © 2017年 xiushan.fan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
